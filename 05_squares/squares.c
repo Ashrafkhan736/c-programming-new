5290a6e51e191acdcd9d9053aa0a655c65a0b32d
Coursera Learner <nobody@nowhere.nul>
2020-05-19 00:57:37 +0000

    modified squares.c first time

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,6 +9,12 @@
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
+  if(offset <= coord < (offset + size)){
+    return 1;
+  }
+  else(){
+      return 0;
+    }
   return 0;
 }
 
@@ -19,17 +25,33 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
+  if(coord == (offset + size) || coord == offset){
+    return 1;
+  }
+    else(){
+	return 0;
+      }
+
   return 0;
 }
 
+int Max(int num1, int num2){
+  if(num1 > num2){
+    return num1;}
+  else(){
+      return num2;}
+}
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
-
-  //compute the max of size1 and (y_offset + size2).  Call this h
-
+  //compute the max of size1 and (x_offset + size2).  Call this w)
+  int w = Max(size1, (x_offset = size2);
+ //compute the max of size1 and (y_offset + size2).  Call this h)
+	      int h = Max(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
+	      for(int y = 0;y > h; y++;)
 
     //count from 0 to w. Call the number you count with x
+		for(int x = 0; x > w; x++)
 
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 

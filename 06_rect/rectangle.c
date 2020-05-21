f91d4b211ba7c2e1b3126a1fefb98fb260cbda6c
Coursera Learner <nobody@nowhere.nul>
2020-05-21 06:12:52 +0000

    code is written and executable file and output is added

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,16 +16,45 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
+struct rect_tag {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rect_tag rectangle;
 
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.width < 0) {
+    r.x = r.x + r.width;
+    r.width = - r.width;
+  }
+  if (r.height < 0) {
+    r.y = r.y + r.height;
+    r.height = - r.height;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
-}
+  rectangle ri;
+  ri = canonicalize(ri);
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  if ( (( (r1.x < r2.x) && ((r1.x + r1.width) < r2.x) ) || ( (r2.x < r1.x) && ((r2.x + r2.width) < r1.x) )) ||(((r1.y<r2.y) &&((r1.y+r1.height) <r2.y)) || ((r2.y<r1.y) &&((r2.y+r2.height)< r1.y)))) {
+    ri.width =0;
+    ri.height=0;
+  }
+  else {
+  ri.x = max(r1.x , r2.x );
+  ri.y = max(r1.y , r2.y );
+  ri.width = min( (r1.x + r1.width) , (r2.x + r2.width) )-ri.x ;
+  ri.height = min( (r1.y +r1.height) , (r2.y + r2.height) ) -ri.y;
+  }
+  return ri;
+  }
 
 //You should not need to modify any code below this line
 void printRectangle(rectangle r) {
